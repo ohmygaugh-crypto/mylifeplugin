48833c8d69cd4dfd79ea4215271ca12e2fceeda7
Seth <58447309+ohmygaugh-crypto@users.noreply.github.com>
2023-08-07 14:35:17 -0400

    1st working attempt seen in styles folder

--- main.ts
+++ main.ts
@@ -1,134 +1,80 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, WorkspaceLeaf, App, ItemView, View, Notice } from 'obsidian';
+import * as d3 from 'd3';
 
-// Remember to rename these classes and interfaces!
+export default class MyLifeInWeeksPlugin extends Plugin {
+    sidebar: LifeInWeeksView | null = null;
 
-interface MyPluginSettings {
-	mySetting: string;
+    async onload() {
+        this.registerView('life-in-weeks-view', (leaf: WorkspaceLeaf) => {
+            if (this.sidebar) return this.sidebar;
+            return this.sidebar = new LifeInWeeksView(leaf);
+        });
+
+        const statusBarItem = this.addStatusBarItem();
+        statusBarItem.setText('Life in Weeks');
+        statusBarItem.onClickEvent(() => {
+            if (this.sidebar) {
+                this.sidebar.leaf.detach();
+                this.sidebar = null;
+            } else {
+                this.sidebar = new LifeInWeeksView(this.app.workspace.splitActiveLeaf());
+            }
+        });
+    }
+
+    onunload() {
+        if (this.sidebar) {
+            this.sidebar.leaf.detach();
+        }
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+class LifeInWeeksView extends ItemView {
+    constructor(leaf: WorkspaceLeaf) {
+        super(leaf);
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        const container = this.containerEl;
+        container.empty();
 
-	async onload() {
-		await this.loadSettings();
+        const width = 400;
+        const height = 1800;
+        const cellSize = 7;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice! hi seth');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const svg = d3.create('svg')
+            .attr('viewBox', [0, 0, width, height]);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        const svgNode = svg.node();
+        if (svgNode) container.appendChild(svgNode);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        const weeks = d3.range(0, 4680).map((d: number) => {
+            return {
+                weekNumber: d,
+                year: Math.floor(d / 52),
+                color: d < 1300 ? 'steelblue' : 'lightgray'
+            };
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        svg.selectAll('rect')
+            .data(weeks)
+            .join('rect')
+            .attr('width', cellSize)
+            .attr('height', cellSize)
+            .attr('x', (d: any, i: number) => (i % 52) * cellSize)
+            .attr('y', (d: any, i: number) => Math.floor(i / 52) * cellSize)
+            .attr('fill', (d: any) => d.color)
+            .on('mouseover', function(event: any, d: any) {
+                d3.select(this).attr('fill', 'red');
+            })
+            .on('mouseout', function(event: any, d: any) {
+                d3.select(this).attr('fill', d.color);
+            });
+    };
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    getDisplayText(): string {
+        return "My Life in Weeks";
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    getViewType(): string {
+        return "life-in-weeks-view";
+    }
 }
